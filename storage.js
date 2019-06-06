302d7ba68e8980c8328c86129b4466bb2cba8282
cwp132 <44931480+cwp132@users.noreply.github.com>
2019-06-06 16:29:10 -0600

    updated projects and tech

--- storage.js
+++ storage.js
@@ -1,32 +1,32 @@
-console.log("ayyo");
+// console.log("ayyo");
 
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyCiGHLM7Y7XJT3je5k2mC_hu9sYhGoVOic",
-    authDomain: "porfolio-contact.firebaseapp.com",
-    databaseURL: "https://porfolio-contact.firebaseio.com/",
-    projectId: "porfolio-contact",
-    storageBucket: "porfolio-contact.appspot.com",
-    messagingSenderId: "682786259399"
-};
+// // Initialize Firebase
+// var config = {
+//     apiKey: "AIzaSyCiGHLM7Y7XJT3je5k2mC_hu9sYhGoVOic",
+//     authDomain: "porfolio-contact.firebaseapp.com",
+//     databaseURL: "https://porfolio-contact.firebaseio.com/",
+//     projectId: "porfolio-contact",
+//     storageBucket: "porfolio-contact.appspot.com",
+//     messagingSenderId: "682786259399"
+// };
 
-firebase.initializeApp(config);
-var database = firebase.database.ref();
+// firebase.initializeApp(config);
+// var database = firebase.database.ref();
 
-$("#submit").on('click', function () {
-    var name = $("#nameInput").val();
-    var email = $("#emailInput").val();
-    var message = $("#messageInput").val();
+// $("#submit").on('click', function () {
+//     var name = $("#nameInput").val();
+//     var email = $("#emailInput").val();
+//     var message = $("#messageInput").val();
 
-    var database = database().ref();
+//     var database = database().ref();
 
-    name.val("");
-    email.val("");
-    message.val("");
+//     name.val("");
+//     email.val("");
+//     message.val("");
 
-    database.ref().push({
-        name: name,
-        email: email,
-        message: message,
-    })
-})
+//     database.ref().push({
+//         name: name,
+//         email: email,
+//         message: message,
+//     })
+// })
